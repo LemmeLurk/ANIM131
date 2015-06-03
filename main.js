e9640008821c48e0cc366049aade86999cc54281
vinxi <vinxi@live.com>
2015-06-02 18:12:57 -0700

    This is a working version that no longer crashes, and of course spawns zombies. Has working stat increaser -- even if the stats should be tweaked, there are way too many zombies by the 20th iteration.

--- main.js
+++ main.js
@@ -1,90 +1,97 @@
+/// <reference path="~/phaser.js" />
+
+
+// Global helper members
+var numberOfZombies = [1, 2, 3, 4, 5];
+var numberWeights = {
+    'first': [0.59, 0.3, 0.07, 0.03, 0.01],
+    'second': [0.49, 0.4, 0.07, 0.03, 0.01],
+    'third': [0.39, 0.4, 0.17, 0.03, 0.01],
+    'fourth': [0.29, 0.4, 0.17, 0.13, 0.01],
+    'fifth': [0.19, 0.4, 0.17, 0.13, 0.11],
+    'sixth': [0.09, 0.5, 0.17, 0.13, 0.11],
+    'seventh': [0.09, 0.4, 0.27, 0.13, 0.11],
+    'eigth': [0.09, 0.3, 0.27, 0.23, 0.11],
+    'ninth': [0.09, 0.2, 0.27, 0.23, 0.21],
+    'tenth': [0.09, 0.1, 0.27, 0.23, 0.31]
+};
+
+var _currentNumberWeights = numberWeights.first;
+
+var _NumberWeightCount = 1;
+
+var typeOfZombies = [1, 2, 3];
+var typeWeights = {
+    'first': [0.60, 0.30, 0.1],
+    'second': [0.30, 0.60, 0.1],
+    'third': [0.1, 0.30, 0.60]
+};
+
+var _currentTypeWeights = typeWeights.first;
+
+var _TypeWeightCount = 1;
+
+var killCounter = 0;
+var zombieCounter = 100;
+
+var angle = 0;
+var fireRate = 500;
+var nextFire = 0;
+
+var rateOfSpawn = 1500;
+
+var dx;
+var dy;
+
+var cloudWidth = 718;
+
 var windowWidth = $(window).width();
 var windowHeight = $(window).height();
 
+// Initialize Phaser, and create a 400x490px game
+//var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 var game = new Phaser.Game(windowWidth, windowHeight, Phaser.AUTO, 'gameDiv');
 
 // Create our 'main' state that will contain the game
 var mainState = {
-    // Define Global Variables
-    numberOfZombies: [1, 2, 3, 4, 5],
-    numberWeights : 
-    {
-        'first': [0.59, 0.3, 0.07, 0.03, 0.01],
-        'second': [0.49, 0.4, 0.07, 0.03, 0.01],
-        'third': [0.39, 0.4, 0.17, 0.03, 0.01],
-        'fourth': [0.29, 0.4, 0.17, 0.13, 0.01],
-        'fifth': [0.19, 0.4, 0.17, 0.13, 0.11],
-        'sixth': [0.09, 0.5, 0.17, 0.13, 0.11],
-        'seventh': [0.09, 0.4, 0.27, 0.13, 0.11],
-        'eigth': [0.09, 0.3, 0.27, 0.23, 0.11],
-        'ninth': [0.09, 0.2, 0.27, 0.23, 0.21],
-        'tenth': [0.09, 0.1, 0.27, 0.23, 0.31]
-    },
-
-    _currentNumberWeights: null,
-
-    _NumberWeightCount: 1,
-
-    typeOfZombies: [1, 2, 3],
-    typeWeights: 
-    {
-        'first': [0.60, 0.30, 0.1],
-        'second': [0.30, 0.60, 0.1],
-        'third': [0.1, 0.30, 0.60]
-    },
-
-    _currentTypeWeights: null,
-
-    _TypeWeightCount: 1,
-
-    killCounter: 0,
-    zombieCounter: 100,
-
-    fireRate: 500,
-    nextFire: 0,
-
-    rateOfSpawn: 1500,
-
-    cloudWidth: 718,
-
 
     preload: function () 
     {
         // Change the background color of the game
-        this.game.stage.backgroundColor = '#71c5cf';
+        game.stage.backgroundColor = '#71c5cf';
 
         // Load the image of Cloud
-        this.game.load.image('cloud', 'assets/cloud.png');
+        game.load.image('cloud', 'assets/cloud.png');
 
         // Load the player sprite
-        this.game.load.image('player', 'assets/man.png');
+        game.load.image('player', 'assets/man.png');
 
-        this.game.load.spritesheet('zombieSpritesheet', 
+        game.load.spritesheet('zombieSpritesheet', 
             'assets/BalloonSpritesheet.png', 50, 150);
 
         // Load the zombie sprite
         //game.load.image('torso', 'assets/Zombie.png');
 
         // Load the Balloon sprites
-        this.game.load.image('OneBalloon', 'assets/OneBalloon.png');
-        this.game.load.image('TwoBalloons', 'assets/TwoBalloons.png');
-        this.game.load.image('ThreeBalloons', 'assets/ThreeBalloons.png');
+        game.load.image('OneBalloon', 'assets/OneBalloon.png');
+        game.load.image('TwoBalloons', 'assets/TwoBalloons.png');
+        game.load.image('ThreeBalloons', 'assets/ThreeBalloons.png');
 
-        this.game.load.image('oneAlone', 'assets/oneAlone.png');
-        this.game.load.image('twoAlone', 'assets/twoAlone.png');
-        this.game.load.image('threeAlone', 'assets/threeAlone.png');
+        game.load.image('oneAlone', 'assets/oneAlone.png');
+        game.load.image('twoAlone', 'assets/twoAlone.png');
+        game.load.image('threeAlone', 'assets/threeAlone.png');
 
-        this.game.load.image('handgun', 'assets/handgun.png');
-        this.game.load.image('bullet', 'assets/projectile.png');
+        game.load.image('handgun', 'assets/handgun.png');
+        game.load.image('bullet', 'assets/projectile.png');
 
-        this.game.load.image('WaveGauge', 'assets/WaveGauge.png');
+        game.load.image('WaveGauge', 'assets/WaveGauge.png');
     },
 
 
     create: function () 
     {
         // Set the physics system
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
         this.cursors = 
             this.game.input.keyboard.createCursorKeys();
@@ -92,7 +99,7 @@ var mainState = {
         /*
         Cloud Object
         */
-        var cloudX = this.game.world.centerX - (this.cloudWidth/2);
+        var cloudX = this.game.world.centerX - (cloudWidth/2);
         this.cloud = this.game.add.sprite(cloudX, 200, 'cloud');
 
         this.game.physics.arcade.enable(this.cloud);
@@ -106,14 +113,14 @@ var mainState = {
         /*
         CONTAINER :: Player, handgun
         */
-        this.container = this.game.add.sprite(0, 0, null);
+        this.container = game.add.sprite(0, 0, null);
 
         //this.game.physics.arcade.enable(this.container);
 
             /*
             Player
             */
-        var playerX = (this.cloudWidth/2) + cloudX;
+        var playerX = (cloudWidth/2) + cloudX;
         this.container.player = this.game.add.sprite(playerX, 210, 'player');
         this.container.player.anchor.setTo(0.5);
         this.container.addChild(this.container.player);
@@ -147,7 +154,7 @@ var mainState = {
         /*
         Zombies :: No Balloons
         */
-        this.zombie = this.game.add.group();
+        this.zombie = game.add.group();
         this.zombie.enableBody = true;
         this.zombie.createMultiple(500, 'zombieSpritesheet', 0);
         this.zombie.setAll('body.gravity.y', 1000);
@@ -158,7 +165,7 @@ var mainState = {
         /*
         Zombies :: 1 Balloon
         */
-        this.oneBalloon = this.game.add.group();
+        this.oneBalloon = game.add.group();
         this.oneBalloon.enableBody = true;
         this.oneBalloon.createMultiple(500, 'zombieSpritesheet', 1);
         this.oneBalloon.forEach(function(zombie){
@@ -168,7 +175,7 @@ var mainState = {
         /*
         Zombies :: 2 Balloons
         */
-        this.twoBalloons = this.game.add.group();
+        this.twoBalloons = game.add.group();
         this.twoBalloons.enableBody = true;
         this.twoBalloons.createMultiple(500, 'zombieSpritesheet', 2);
         this.twoBalloons.forEach(function(zombie){
@@ -178,7 +185,7 @@ var mainState = {
         /*
         Zombies :: 3 Balloons
         */
-        this.threeBalloons = this.game.add.group();
+        this.threeBalloons = game.add.group();
         this.threeBalloons.enableBody = true;
         this.threeBalloons.createMultiple(500, 'zombieSpritesheet', 3);
         this.threeBalloons.forEach(function(zombie){
@@ -189,17 +196,17 @@ var mainState = {
         /*
         Balloons and Zombies Without Heads
         */
-        this.oneAlone = this.game.add.group();
+        this.oneAlone = game.add.group();
         this.oneAlone.enableBody = true;
         this.oneAlone.createMultiple(50, 'oneAlone');
         this.oneAlone.setAll('body.velocity.y', -20);
 
-        this.twoAlone = this.game.add.group();
+        this.twoAlone = game.add.group();
         this.twoAlone.enableBody = true;
         this.twoAlone.createMultiple(50, 'twoAlone');
         this.twoAlone.setAll('body.velocity.y', -40);
 
-        this.threeAlone = this.game.add.group();
+        this.threeAlone = game.add.group();
         this.threeAlone.enableBody = true;
         this.threeAlone.createMultiple(50, 'threeAlone');
         this.threeAlone.setAll('body.velocity.y', -60);
@@ -208,7 +215,7 @@ var mainState = {
         /*
         Bullets
         */
-        this.bullets = this.game.add.group();
+        this.bullets = game.add.group();
         this.bullets.createMultiple(50, 'bullet', 0, false);
         this.bullets.forEach(function(bullet){
             bullet.enableBody = true;
@@ -221,7 +228,7 @@ var mainState = {
         Timers
         */
        // this.timer = game.time.events.loop(3500, this.addZombieHorde, this);
-        this.timer = this.game.time.events.loop(this.rateOfSpawn, 
+        this.timer = game.time.events.loop(rateOfSpawn, 
             this.addZombieHorde, this);
 
 
@@ -234,7 +241,7 @@ var mainState = {
         // Add timer :: Increase stats every 1.5sec
         this.gameplayTimer = 
             //game.time.events.loop(10000, this.increaseStats, this);
-            this.game.time.events.loop(5000, this.increaseStats, this);
+            game.time.events.loop(5000, this.increaseStats, this);
 
 
         /*
@@ -242,7 +249,7 @@ var mainState = {
         */ 
         this.score = 0;
         
-        this.labelScore = this.game.add.text(20, 20, "0", {
+        this.labelScore = game.add.text(20, 20, "0", {
             font:
                 "30px Arial", fill: "#ffffff" });
 
@@ -252,7 +259,7 @@ var mainState = {
         this.shotCounter = 6;
 
         this.labelShotCounter = 
-            this.game.add.text(200, 200, "6", 
+            game.add.text(200, 200, "6", 
                 {font: "30px Arial", fill: "#ffffff"});
 
         /*
@@ -273,18 +280,15 @@ var mainState = {
 
     update: function () 
     {
-        /*
-        GAME OVER
-        */
-        if (this.killCounter === 100 || (!this._currentTypeWeights && !this._currentNumberWeights))
+        if (killCounter === 100)
         {
-            this.restartGame();
+            alert('Game Over');
         }
         /*
         Aiming / Shooting
         */
-        var dx = this.game.input.activePointer.worldX - this.container.handgun.x;
-        var dy = this.game.input.activePointer.worldY - this.container.handgun.y;
+        dx = this.game.input.activePointer.worldX - this.container.handgun.x;
+        dy = this.game.input.activePointer.worldY - this.container.handgun.y;
 
         this.container.handgun.rotation = Math.atan2(dy, dx);
 
@@ -353,33 +357,7 @@ var mainState = {
     restartGame: function () 
     {
         // Start the 'main'state, which restarts the game
-        /*
-        RESET Global Variables to Default Values
-        */
-        this._TypeWeightCount = 0;
-        this._currentTypeWeights = this.typeWeights.first;
-
-        this._NumberWeightCount = 0;
-        this._currentNumberWeights = this.numberWeights.first;
-
-        this.rateOfSpawn = 1500;
-
-        this.killCounter = 0;
-        this.zombieCounter = 100;
-
-        /*
-        RESET Label's Text
-        */
-        this.score = 0;
-        this.labelScore.text = "0";
-
-        /*
-        RESET WaveGauge Size
-        */
-        this.WaveGauge.width = 100;
-        // Does this next call conflict, override and apply values
-        // that I have set? Is some above code redundant?
-        this.game.state.start('main');
+        game.state.start('main');
     },
 
 
@@ -558,19 +536,19 @@ var mainState = {
             return weighedList[R_Index];
         };
 
-        R_NumberOfZombies = randomGenerator(this._currentNumberWeights, this.numberOfZombies);
-        this.zombieCounter -= R_NumberOfZombies;
+        R_NumberOfZombies = randomGenerator(_currentNumberWeights, numberOfZombies);
+        zombieCounter -= R_NumberOfZombies;
 
         var _currentNumberOfZombies = 0;
-        for (; this._currentNumberOfZombies < R_NumberOfZombies;)
+        for (; _currentNumberOfZombies < R_NumberOfZombies;)
         {
             var xCoord = Math.floor(Math.random() * (windowWidth - 300)) + 150;
             var yCoord = Math.floor(Math.random() * (windowHeight - 150)) + 300;
 
-            R_TypeOfZombie = randomGenerator(this._currentTypeWeights, typeOfZombies)
+            R_TypeOfZombie = randomGenerator(_currentTypeWeights, typeOfZombies)
 
             this.addOneZombie(xCoord, yCoord + 10, R_TypeOfZombie);
-            this._currentNumberOfZombies++;
+            _currentNumberOfZombies++;
         }
 
         // Increment the score by 1 ea time new pipes are created
@@ -588,62 +566,62 @@ var mainState = {
         this.twoBalloons.rate *= 0.5;
         this.threeBalloons.rate *= 0.5;
 
-        switch (this._NumberWeightCount)
+        switch (_NumberWeightCount)
         {
             case 1:
-                this._currentNumberWeights = this.numberWeights.second;  
+                _currentNumberWeights = numberWeights.second;  
             break;
 
             case 2:
-                this._currentNumberWeights = this.numberWeights.third;  
+                _currentNumberWeights = numberWeights.third;  
             break;
 
             case 3:
-                this._currentNumberWeights = this.numberWeights.fourth;  
-                this._currentTypeWeights = this.typeWeights.second;
+                _currentNumberWeights = numberWeights.fourth;  
+                _currentTypeWeights = typeWeights.second;
             break;
 
             case 4:
-                this._currentNumberWeights = this.numberWeights.fifth;  
+                _currentNumberWeights = numberWeights.fifth;  
             break;
 
             case 5:
-                this._currentNumberWeights = this.numberWeights.sixth;  
+                _currentNumberWeights = numberWeights.sixth;  
             break;
 
             case 6:
-                this._currentNumberWeights = this.numberWeights.seventh;  
-                this._currentTypeWeights = this.typeWeights.third;
+                _currentNumberWeights = numberWeights.seventh;  
+                _currentTypeWeights = typeWeights.third;
             break;
 
             case 7:
-                this._currentNumberWeights = this.numberWeights.eigth;  
+                _currentNumberWeights = numberWeights.eigth;  
             break;
 
             case 8:
-                this._currentNumberWeights = this.numberWeights.ninth;  
+                _currentNumberWeights = numberWeights.ninth;  
             break;
 
             case 9:
-                this._currentNumberWeights = this.numberWeights.tenth;  
+                _currentNumberWeights = numberWeights.tenth;  
             break;
         }
 
-        this._NumberWeightCount++;
+        _NumberWeightCount++;
     },
 
 
     shoot: function () 
     {
-        if (this.game.time.now > this.nextFire)
+        if (game.time.now > nextFire)
         {
-            this.nextFire = this.game.time.now + this.fireRate;
+            nextFire = game.time.now + fireRate;
 
             var bullet = this.bullets.getFirstExists(false);
 
             if (bullet)
             {
-                bullet.frame = this.game.rnd.integerInRange(0, 6);
+                bullet.frame = game.rnd.integerInRange(0, 6);
                 bullet.exists = true;
                 bullet.position.set(
                     this.container.handgun.x, 
